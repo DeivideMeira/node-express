233f4f5b195abd50fdb7538ba1f86548d9138483
Deivide Meira <meiradeivide@gmail.com>
2022-01-05 18:54:14 -0300

    alteração

--- node/app.js
+++ node/app.js
@@ -1,41 +1,33 @@
 const http = require('http')
-
 const server = http.createServer((req, res) => {
+let josé;
 
   if (req.url === '/') {
     res.end(`
     <h1>Welcome!</h1>
-    <h2>Please entire your name:</h2>
-    <input type="text" value="name"/> </br></br>
-    <h2>Please entire your year of age:</h2>
-    <input type="number" value="age"/> </br></br>
-    <button type="submit"></button>
     `)
   } 
   
   else {
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `)
+    res.end()
   }
 })
 
 function generateBornDateFromAge(age) {
-    return 2016 - age;
-   }
-    
-    
-   function generateUserDescription(name, age) {
-    const fullName = name + " ";
-    const bornDate = generateBornDateFromAge(age);
-    
-    
-    return fullName + " is " + age + " old and was born in " + bornDate;
-   }
-    
-    
-   generateUserDescription("Waldemar", 26);
+  return 2016 - age;
+ }
+  
+  
+ function generateUserDescription(name, age) {
+  const Name = name;
+  const bornDate = generateBornDateFromAge(age);
+  
+  
+  return Name + " is " + age + " old and was born in " + bornDate;
+ }
+  
+  
+let UserData = generateUserDescription("Waldemar", 26);
+console.log(UserData);
 
 server.listen(3000)
