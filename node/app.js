8949a00317e4b5f3ca3c79d7b2a80e888e2d58c0
Deivide Meira <meiradeivide@gmail.com>
2022-01-11 17:26:06 -0300

    add changes on create big-files

--- node/app.js
+++ node/app.js
@@ -1,21 +1,5 @@
-const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
 
-const start = async () => {
-  try {
-    const first = await readFile('./content/first.txt', 'utf8')
-    const second = await readFile('./content/second.txt', 'utf8')
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `Incrível! \n\nPrimeiro temos: ${first} \n\ne depois: ${second}`,
-      { flag: 'a' }
-    )
-    console.log(first, second)
-  } catch (error) {
-    console.log(error)
-  }
+const { writeFileSync } = require('fs')
+for (let i = 0; i < 10000; i++) {
+  writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
 }
-
-start()
