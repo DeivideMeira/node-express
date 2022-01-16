7ff20f7f24fe34de101166e54c1622b3f2cd8c2a
Deivide Meira <meiradeivide@gmail.com>
2022-01-16 17:50:21 -0300

    new commit

--- express/app.js
+++ express/app.js
@@ -4,7 +4,11 @@ const {readFileSync} = require('fs')
 //res.writeHead (statusCode, {statusMessage - o browser já põe, mas pode personalizar} {headers})
 
 //all status code https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./content/index.html')
+const homeStyles = readFileSync('./content/styles.css')
+const homeImage= readFileSync('./content/logo.svg')
+const homeLogic = readFileSync('./content/browser-app.js')
+
 
 const server = http.createServer((req, res) => {
     console.log('usuário atingiu o servidor');
@@ -17,11 +21,25 @@ const server = http.createServer((req, res) => {
         res.write(homePage)
         res.end()
     }
-    else if(url === '/about'){
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h1>about page</h1>')
+
+    else if(url === '/styles.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyles)
         res.end()
     }
+
+    else if(url === '/logo.svg'){
+        res.writeHead(200, {'content-type':'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+
+    else if(url === '/browser-app.js'){
+        res.writeHead(200, {'content-type':'image/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
+
     else{
         res.writeHead(404, {'content-type':'text/html'}) 
         res.write('<h1>page not found</h1>')
